7fbf95d4f8bdbf293a8ffa350929e88af21e5364
Lucas Parzianello <lucaspar@users.noreply.github.com>
2025-08-12 12:47:44 -0400

    openai stream demo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Usage example for this package."""
 
 from loguru import logger as log
+import numpy as np
 
 import uv_demo
 
@@ -10,6 +11,13 @@ def main() -> None:
     log.info(uv_demo.__version__)
     log.info(uv_demo.LIB_NAME)
     uv_demo.say_hello()
+
+    center_frequencies = np.array([0.0] * 4)
+    log.info(center_frequencies)
+    log.info(type(center_frequencies))
+    log.info(center_frequencies.dtype)
+    myfloat = 0.0
+    log.info(type(myfloat))
 
 
 if __name__ == "__main__":
